8ec777869572306fa065d65c102d7f8494afca27
2ez2playroblox <113609022+helloeveryonehoware@users.noreply.github.com>
2022-12-21 09:05:57 -0600

    Update main.py

--- Just making stuff/main.py
+++ Just making stuff/main.py
@@ -4,9 +4,14 @@ import json
 import os
 import
 
-os.chdir("C:\\Users\\S1980253\\Downloads\\Just making stuff")
+os.chdir("C:\\Users\\S1980253\\Downloads\\My discord bot main\\Just making stuff")
 
 client = commands.Bot(command_prefix = ">")
+
+
+@client.event
+async def on_ready():
+    print(f"{bot.username} is ready!!")
 
 @client.command()
 async def balance(ctx):
@@ -25,6 +30,44 @@ async def balance(ctx):
 
 @client.command()
 async def beg(ctx):
+    await open_account(ctx.author)
+
+    users = await get_bank_data()
+
+    earnings = random.randrange(101) # you can change the number to what you want
+
+    await ctx.send(f"Someone gave you {earnings} coins!!")
+
+
+    users[str(user.id)]["wallet"] += earnings
+
+    with open("mainbank.json", "w") as f:
+        json.dump(users,f)
+
+
+@client.command()
+async def withdraw(ctx,amount = None):
+    await open_account(ctx.author)
+
+
+    if amount == None:
+        await ctx.sned("what do you want to withdraw dumb idiot")
+        return
+
+    bal = await update_bank(ctx.author)
+
+
+    if amount>bal[1]:
+        await ctx.send("you don't have that much money idiot get a job")
+        return
+    if amount<0:
+        await ctx.send("you idiot it needs to be more than 1")
+        return
+        
+        await update_bank(ctx.author,amount)
+        await update_bank(ctx.author, -1*amount,"bank")
+
+
 
 
     with open("mainbank.json","r") as f:
@@ -39,6 +82,7 @@ async def open_account(user):
     if str(user.id) in users:
         return False
     else:
+        users[str(user.id)] = {}
         users[str(user.id)]["wallet"] = 0
         users[str(user.id)]["bank"] = 0
 
@@ -48,4 +92,18 @@ async def open_account(user):
 
 
 async def get_bank_data():
-    
+    with open("mainbank.json","r") as f:
+        users = json.load(f)
+
+    return users
+
+
+async def update_bank(user,change = 0,mode = "wallet")
+    users = await get_bank_data()
+
+    users[str(user.id)][mode] += change
+
+    with open("mainbank.json","w") as f:
+        json.dump(users,f)
+
+    bal = [users[str(user.id)]["wallet"],users[str(user.id)]["bank"]]
